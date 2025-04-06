0a564304e601da5008063bb682ea553b26594b2e
Himanshu Raj <hraj808404@gmail.com>
2025-04-06 11:43:50 +0530

    hj

---
+++ server/controllers/educatorController.js
@@ -0,0 +1,17 @@
+
+
+
+export const updateRoleToEducator=async()=>{
+    try{
+        const userId=req.auth.userId
+        await clerkClient.users.updateUserMetadata(userId,{
+            publicMetadata:{
+                role:'educator',
+            }
+        })
+        res.json({success:true,message:'You can publish a course now'})
+
+    }catch(error){
+        res.json({success:false,message:error.message})
+    }
+}
