e07f9086dd5398b0914ef83415a2e8eb2c65ee29
Himanshu Raj <hraj808404@gmail.com>
2025-04-06 11:22:13 +0530

    env var updated

--- server/server.js
+++ server/server.js
@@ -3,24 +3,18 @@ import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './configs/mongodb.js'
 import { clerkWebhooks } from './controllers/webhooks.js'
-//initialise express
+
 const app = express()
 
-//connect to database
-await connectDB()
 
-// Middlewares
+await connectDB()
 app.use(cors())
 
-// Health check route
 app.get('/', (req, res) => {
-    res.send("API Working" )
+    res.send("API Working")
 })
+app.post('/clerk', express.json(), clerkWebhooks)
 
-// Webhook route
-app.post('/clerk',express.json(), clerkWebhooks)
-
-//ports
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
