a1d75f0c0622a1f3b348622de07047ca465c617e
Himanshu Raj <hraj808404@gmail.com>
2025-04-11 00:18:05 +0530

    backend completed

--- server/models/courseProgress.js
+++ server/models/courseProgress.js
@@ -1,11 +1,11 @@
-// import mongoose from 'mongoose'
+import mongoose from 'mongoose'
 
-// const courseProgressSchema=new mongoose.Schema({
-//     userId:{type:String,required:true},
-//     courseId:{type:String,required:true},
-//     completed:{type:Boolean,default:false},
-//     lectureCompleted:[]
+const courseProgressSchema=new mongoose.Schema({
+    userId:{type:String,required:true},
+    courseId:{type:String,required:true},
+    completed:{type:Boolean,default:false},
+    lectureCompleted:[]
 
-// },{minimize:false});
+},{minimize:false});
 
-// export const CourseProgress=mongoose.model('CourseProgress',courseProgressSchema)
+export const CourseProgress=mongoose.model('CourseProgress',courseProgressSchema)
