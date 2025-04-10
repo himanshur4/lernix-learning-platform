0af0357bf3f052ad0af415f0943417cb85f1b9f0
Himanshu Raj <hraj808404@gmail.com>
2025-04-10 14:46:31 +0530

    backend debugging1

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -1,55 +1,55 @@
-// import { Webhook } from "svix";
-// import User from "../models/user.js";
+import { Webhook } from "svix";
+import User from "../models/user.js";
 // import Stripe from "stripe";
 // import Purchase from "../models/purchase.js";
 // import Course from "../models/course.js";
 
-// //API controller function to manage clerk user with database
+//API controller function to manage clerk user with database
 
-// export const clerkWebhooks = async (req, res) => {
-//     try {
-//         const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET)
+export const clerkWebhooks = async (req, res) => {
+    try {
+        const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET)
 
-//         await whook.verify(JSON.stringify(req.body), {
-//             "svix-id": req.headers["svix-id"],
-//             "svix-timestamp": req.headers["svix-timestamp"],
-//             "svix-signature": req.headers["svix-signature"]
-//         })
-//         const { data, type } = req.body
-//         switch (type) {
-//             case 'user.created': {
-//                 const userData = {
-//                     _id: data.id,
-//                     email: data.email_addresses[0].email_address,
-//                     name: data.first_name + " " + data.last_name,
-//                     imageUrl: data.image_url,
-//                 }
-//                 await User.create(userData)
-//                 res.json({})
-//                 break;
-//             }
-//             case 'user.updated': {
-//                 const userData = {
-//                     email: data.email_addresses[0].email_address,
-//                     name: data.first_name + " " + data.last_name,
-//                     imageUrl: data.image_url,
-//                 }
-//                 await User.findByIdAndUpdate(data.id)
-//                 res.json({})
-//                 break;
-//             }
-//             case 'user.deleted': {
-//                 await User.findByIdAndDelete(data.id)
-//                 res.json({})
-//                 break;
-//             }
-//             default:
-//                 break;
-//         }
-//     } catch (error) {
-//         res.json({ success: false, message: error.message })
-//     }
-// }
+        await whook.verify(JSON.stringify(req.body), {
+            "svix-id": req.headers["svix-id"],
+            "svix-timestamp": req.headers["svix-timestamp"],
+            "svix-signature": req.headers["svix-signature"]
+        })
+        const { data, type } = req.body
+        switch (type) {
+            case 'user.created': {
+                const userData = {
+                    _id: data.id,
+                    email: data.email_addresses[0].email_address,
+                    name: data.first_name + " " + data.last_name,
+                    imageUrl: data.image_url,
+                }
+                await User.create(userData)
+                res.json({})
+                break;
+            }
+            case 'user.updated': {
+                const userData = {
+                    email: data.email_addresses[0].email_address,
+                    name: data.first_name + " " + data.last_name,
+                    imageUrl: data.image_url,
+                }
+                await User.findByIdAndUpdate(data.id)
+                res.json({})
+                break;
+            }
+            case 'user.deleted': {
+                await User.findByIdAndDelete(data.id)
+                res.json({})
+                break;
+            }
+            default:
+                break;
+        }
+    } catch (error) {
+        res.json({ success: false, message: error.message })
+    }
+}
 // const stripeInstance = new Stripe(process.env.STRIPE_SECRET_KEY)
 // export const stripeWebhooks = async (req, res) => {
 //     const sig = req.headers['stripe-signature']
--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
 import express from 'express'
-// import cors from 'cors'
+import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './configs/mongodb.js'
-// import { clerkWebhooks, stripeWebhooks } from './controllers/webhooks.js'
+import { clerkWebhooks } from './controllers/webhooks.js'
 // import educatorRouter from './routes/educatorRoutes.js'
 // import { clerkMiddleware } from '@clerk/express'
 // import connectCloudinary from './configs/cloudinary.js'
@@ -13,14 +13,14 @@ import connectDB from './configs/mongodb.js'
 const app=express()
 await connectDB()
 // await connectCloudinary()
-// //middlewares
-// app.use(cors())
+//middlewares
+app.use(cors())
 // app.use(clerkMiddleware())
 
 //routes
 app.get('/',(req,res)=>res.send("API Working"))
 
-// app.post('/clerk',express.json(),clerkWebhooks)
+app.post('/clerk',express.json(),clerkWebhooks)
 
 // app.use('/api/educator',express.json(),educatorRouter)
 
