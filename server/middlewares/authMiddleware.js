dd93c8141f95cba70e1e68b258bcc39ef9fb83be
Himanshu Raj <hraj808404@gmail.com>
2025-04-10 13:20:31 +0530

    backend debugging

--- server/middlewares/authMiddleware.js
+++ server/middlewares/authMiddleware.js
@@ -1,16 +1,16 @@
-import { clerkClient } from "@clerk/express";
+// import { clerkClient } from "@clerk/express";
 
 
-//Middleware (Protect Educator Routes)
+// //Middleware (Protect Educator Routes)
 
-export const protectEducator = async (req, res, next) => {
-    try {
-        const userId = req.auth.userId;
-        const response = await clerkClient.users.getUser(userId)
-        if (response.publicMetadata.role !== 'educator') return res.json({ success: false, message: 'Unauthorized Access' })
+// export const protectEducator = async (req, res, next) => {
+//     try {
+//         const userId = req.auth.userId;
+//         const response = await clerkClient.users.getUser(userId)
+//         if (response.publicMetadata.role !== 'educator') return res.json({ success: false, message: 'Unauthorized Access' })
 
-        next()
-    } catch (error) {
-        res.json({ success: false, message: error.message })
-    }
-}
+//         next()
+//     } catch (error) {
+//         res.json({ success: false, message: error.message })
+//     }
+// }
