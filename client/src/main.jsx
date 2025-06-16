e13f830dafc939a87f8fe160bb1c66175d0c4902
Himanshu Raj <hraj808404@gmail.com>
2025-06-16 22:47:07 +0530

    bg change

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -15,9 +15,11 @@ createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl='/'>
       <AppContextProvider>
-       <div className="absolute top-0 z-[-2] min-h-screen min-w-screen max-w-7xl max-h-fit transform bg-white bg-[radial-gradient(60%_120%_at_50%_50%,hsla(0,0%,100%,0)_0,rgba(252,205,238,.5)_100%)]">
-        <App />
+        
+        <div className="absolute top-0 z-[-2] min-h-screen min-w-screen max-w-7xl max-h-fit transform bg-white bg-[radial-gradient(60%_120%_at_50%_50%,hsla(0,0%,100%,0)_0,rgba(252,205,238,.5)_100%)]">
+          <App />
         </div>
+
       </AppContextProvider>
     </ClerkProvider>
   </BrowserRouter>
